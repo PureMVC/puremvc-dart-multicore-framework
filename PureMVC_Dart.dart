7a3713346f593a96538b2c4ede75d434a8eeace1
Cliff Hall <cliff@futurescale.com>
2012-09-29 13:03:35 -0400

    Prepping for the pub
    added README.md
    fixed deprecated catch clause

--- PureMVC_Dart.dart
+++ PureMVC_Dart.dart
@@ -6,7 +6,7 @@ class PureMVC_Dart {
   PureMVC_Dart() {
   }
 
-  void test() 
+  void test()
   {
     String multitonKey = "Test Core";
     String dataPoint1 = "Hello";
@@ -14,30 +14,30 @@ class PureMVC_Dart {
     String proxyName = "DataProxy";
     List<String> retrievedObject;
     String badJuju = "";
-    
+
     try {
       // Get a Facade
       IFacade facade = MVCFacade.getInstance( multitonKey );
       write ("Facade created for ${multitonKey}.");
-      
-      // Create some data 
+
+      // Create some data
       List<String> dataObject = new List<String>();
       write ("Data Object (List&ltString&gt) created.");
       dataObject.add(dataPoint1);
       write ("Data point added '${dataPoint1}'.");
       dataObject.add(dataPoint2);
       write ("Data point added '${dataPoint2}'.");
-      
+
       // Register a Proxy to hold the data
       IProxy proxy = new MVCProxy( proxyName, dataObject );
       write( "Proxy '${proxyName}' created with for Data Object.");
       facade.registerProxy( proxy );
       write ("Proxy '${proxyName}' registered with Model, via Facade.");
-      
+
       // Now retrieve the Proxy
       IProxy retrievedProxy = facade.retrieveProxy( proxyName );
       write ("Proxy '${proxyName}' retrieved from Model, via Facade.");
-      
+
       // And get the data
       retrievedObject = retrievedProxy.getData();
       write ("Data Object (List&ltString&gt) retrieved from ${proxyName}");
@@ -46,15 +46,15 @@ class PureMVC_Dart {
 
       // Prove errors will be reported
       // throw "Fungers! I've got jelly in my ears!";
-      
-    } catch (var e) {
+
+    } on Error catch ( e ) {
       // Catch any error
       badJuju = e.toString();
-      
+
     } finally {
-      // Report final status 
-      if ( badJuju.length == 0 && retrievedObject != null 
-           && retrievedObject[0] == dataPoint1 
+      // Report final status
+      if ( badJuju.length == 0 && retrievedObject != null
+           && retrievedObject[0] == dataPoint1
            && retrievedObject[1] == dataPoint2 ){
         write( "<P/><B>Science!</B> PureMVC is purring like a kitten. Take her out for a spin!");
       } else {
@@ -62,7 +62,7 @@ class PureMVC_Dart {
         if( badJuju.length>0 ) write( "<B>Bad juju reported:</B> ${badJuju}");
       }
     }
-    
+
   }
 
   void write(String message) {
---
+++ README.md
@@ -0,0 +1,16 @@
+## [PureMVC](http://puremvc.github.com/) Dart MultiCore Framework
+PureMVC is a lightweight framework for creating applications based upon the classic [Model-View-Controller](http://en.wikipedia.org/wiki/Model-view-controller) design meta-pattern. This is a Dart port of the [AS3 reference implementation of the MultiCore Version](https://github.com/PureMVC/puremvc-as3-multicore-framework/wiki). It supports [modular programming](http://en.wikipedia.org/wiki/Modular_programming) through the use of [Multiton](http://en.wikipedia.org/wiki/Multiton) Core actors.
+
+* [API Docs](http://darkstar.puremvc.org/content_header.html?url=http://puremvc.org/pages/docs/Dart/multicore/puremvc.html&desc=PureMVC%20API%20Docs:%20PureMVC%20MultiCore%20for%20Dart)
+* [Unit Tests](https://github.com/PureMVC/puremvc-dart-multicore-unittests/wiki)
+* [Discussion](http://forums.puremvc.org/index.php?board=98.0)
+* [Overview Presentation](http://puremvc.tv/#P002)
+
+## Demos
+* [Reverse Text](https://github.com/PureMVC/puremvc-dart-demo-reversetext/wiki)
+
+## Status
+Production - [Version 1.3](https://github.com/PureMVC/puremvc-dart-multicore-framework/blob/master/version.txt)
+
+## Platforms / Technologies
+* [Google Dart](http://www.dartlang.org)
